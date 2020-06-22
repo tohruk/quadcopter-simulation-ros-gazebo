99c1adfd6d249700832934006b82c32853bc946b
tohruk <93125+tohruk@users.noreply.github.com>
2020-06-22 21:57:35 +0900

    Update to ROS Melodic

--- Gazebo/setup.sh
+++ Gazebo/setup.sh
@@ -5,12 +5,12 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 sudo apt update
-sudo apt install ros-kinetic-desktop -y
+sudo apt install ros-melodic-desktop -y
 
 sudo rosdep init
 rosdep update
 
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 sudo apt install python-rosinstall -y
@@ -21,16 +21,16 @@ catkin_make
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt install ros-kinetic-joy -y
-sudo apt install ros-kinetic-image-view -y
+sudo apt install ros-melodic-joy -y
+sudo apt install ros-melodic-image-view -y
 
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 sudo apt update
 
-sudo apt install gazebo7 libgazebo7-dev
-sudo apt install ros-kinetic-gazebo-ros-pkgs ros-kinetic-gazebo-ros-control
-sudo apt install ros-kinetic-hector-gazebo-plugins ros-kinetic-hector-sensors-description ros-kinetic-hector-gazebo
+sudo apt install gazebo9 libgazebo9-dev
+sudo apt install ros-melodic-gazebo-ros-pkgs ros-melodic-gazebo-ros-control
+sudo apt install ros-melodic-hector-gazebo-plugins ros-melodic-hector-sensors-description ros-melodic-hector-gazebo
 
 cd ~/catkin_ws/src/IMAV_2017_Virtual_Challenge/plugins
 mkdir -p build
